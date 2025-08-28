df0f94111df015d358d5a0ed836405964157645f
SolarpunkTaskforce <brosenbauer.m@icloud.com>
2025-08-28 10:13:09 +0200

    Make map full screen and refine navigation menus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,7 @@ const Map = dynamic(() => import("@/components/Map"), { ssr: false });
 
 export default function HomePage() {
   return (
-    <main className="p-4">
-      <div className="mb-4 text-xl font-semibold">Solarpunk Taskforce</div>
+    <main className="h-[calc(100vh-3.5rem)] w-full">
       <Map markers={[]} />
     </main>
   );
